f9e7ded1760d299877098b8eff50b58435896b3e
Alex Bonwit <alexbonwit@mac.com>
2020-02-24 08:15:00 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form');
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    let newTask = document.getElementById('new-task-description');
+    let taskList = document.getElementById('tasks');
+
+    let newListItem = document.createElement("li");
+    newListItem.innerText = newTask.value;
+
+    let deleteButton = document.createElement('button');
+    deleteButton.innerText = "x";
+    newListItem.append(deleteButton);
+    deleteButton.addEventListener('click', event => deleteTask(event));
+
+    taskList.appendChild(newListItem);
+    form.reset();
+  })
+
+  function deleteTask(event) {
+    let removedTask = event.target.parentNode;
+    removedTask.remove();
+  }
+
 });
